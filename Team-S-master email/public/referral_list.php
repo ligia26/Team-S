1cfa421f6db109773c55dac23e973ca0f924a7c5
K1895306 <47645897+k1895306@users.noreply.github.com>
2020-04-02 10:48:58 +0530

    Update referral_list.php
    Referral list css

--- Team-S-master email/public/referral_list.php
+++ Team-S-master email/public/referral_list.php
@@ -15,7 +15,7 @@ if (isset($_POST['submitbtn'])) {
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <div class= "Referrals listing">
-    <h1> Referrals  </h1>
+    <h1 id="title-page"> REFERRALS  </h1>
     <form method="post" class="example" id="searchbar" action="referral_list.php" style="margin:auto;max-width:700px">
                     <input type="text" name="search" id="searchinput" placeholder="Enter NHS Number to Search">
                     <button name="submitbtn" id="searchbutton" type="submit"><i class="fa fa-search"></i></button>
@@ -24,11 +24,11 @@ if (isset($_POST['submitbtn'])) {
    <br>
    <center>
     <table class= "list">
-        <th> Name </th>
-        <th> NHS number </th>
-        <th> Date of Birth </th>
-        <th> Email </th>
-        <th> Referring Doctor Name </th>
+        <th id = "darkblue"> Name </th>
+        <th id = "lightblue"> NHS number </th>
+        <th id = "darkblue"> Date of Birth </th>
+        <th id = "lightblue"> Email </th>
+        <th id = "darkblue"> Referring Doctor Name </th>
     
 
         <?php while ($patient = mysqli_fetch_assoc($patient_set)){ ?>
@@ -46,6 +46,27 @@ if (isset($_POST['submitbtn'])) {
 
     </div>
 
+    <style>
+    a {
+                background-color: white;
+    box-shadow: -1 1px 0 blue;
+    color: rgb(42,103,204);
+    padding: 0.3em 1em;
+    position: relative;
+    text-decoration: none;
+    /* text-transform: uppercase; */
+} 
+
+a:hover {
+  background-color: rgb(19, 26, 102);
+  cursor: pointer;
+}
+
+a:active {
+  box-shadow: none;
+  top: 5px;
+}
+
+</style>
+
 <?php include(SHARED_PATH . '/footer.php'); ?>
-
-
